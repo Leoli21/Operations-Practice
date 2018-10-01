f72f8b15e34295147887b5d06172fda2fe640731
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2018-10-01 08:32:42 -0400

    Add files via upload

---
+++ src/OperationsPractice.java
@@ -0,0 +1,22 @@
+
+public class OperationsPractice 
+{
+
+	public static void main(String[] args) 
+	{
+		int number;
+		number = 1;
+		System.out.println("Number: " + number);
+		number += 5;
+		System.out.println("Add 5: " + number);
+		number /= 2;
+		System.out.println("Divide value by 2: " + number);
+		number *= 4;
+		System.out.println("Multiply value by 4: " + number);
+		number -= 6;
+		System.out.println("Subtract value by 6: " + number);
+		number %= 2;
+		System.out.println("Modularly divide by 2: " + number);
+	}
+
+}
